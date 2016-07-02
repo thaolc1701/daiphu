b26a77cc04315f56ccac67fe443ac2283b3a6409
ThaoLC <thaolc1701@gmail.com>
2016-07-02 22:46:13 +0700

    create layout home-slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+var general = {
+
+};
+
+var home = {
+  homeCarousel: function(){
+    $('.home-carousel').owlCarousel({
+      loop:true,
+      margin: 0,
+      nav:false,
+      items: 1,
+      autoplay:true,
+      autoplayTimeout:5000,
+      autoplayHoverPause:true
+    })
+  },
+  homeInfoCarousel: function(){
+    $('.home-info-carousel').owlCarousel({
+      loop:true,
+      margin: 0,
+      nav:true,
+      items: 1,
+      dots: false
+    })
+  }
+};
+
 $(document).ready(function() {
-	
+	home.homeCarousel();
+  home.homeInfoCarousel();
 });
