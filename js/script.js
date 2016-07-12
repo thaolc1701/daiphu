616117845ab3d30e85befb2a95affae350bd2a21
Le Chi Thao <th_le@visionet.local>
2016-07-12 16:40:11 +0700

    add slider product image

--- js/script.js
+++ js/script.js
@@ -103,6 +103,76 @@ var home = {
 };
 
 var product = {
+  initImages: function(){
+    var sync1 = $(".slider");
+    var sync2 = $(".thumbnails");
+   
+    sync1.owlCarousel_1({
+      singleItem : true,
+      slideSpeed : 1000,
+      navigation: true,
+      pagination:false,
+      afterAction : syncPosition,
+      responsiveRefreshRate : 200,
+    });
+   
+    sync2.owlCarousel_1({
+      items : 4,
+      itemsDesktop      : [1199,4],
+      itemsDesktopSmall     : [979,4],
+      itemsTablet       : [768,4],
+      itemsMobile       : [479,4],
+      pagination:false,
+      responsiveRefreshRate : 100,
+      afterInit : function(el){
+        el.find(".owl-item").eq(0).addClass("synced");
+      }
+    });
+   
+    function syncPosition(el){
+      var current = this.currentItem;
+      $(".thumbnails")
+        .find(".owl-item")
+        .removeClass("synced")
+        .eq(current)
+        .addClass("synced")
+      if($(".thumbnails").data("owlCarousel_1") !== undefined){
+        center(current)
+      }
+    }
+   
+    $(".thumbnails").on("click", ".owl-item", function(e){
+      e.preventDefault();
+      var number = $(this).data("owlItem");
+      sync1.trigger("owl.goTo",number);
+    });
+   
+    function center(number){
+      var sync2visible = sync2.data("owlCarousel_1").owl.visibleItems;
+      var num = number;
+      var found = false;
+      for(var i in sync2visible){
+        if(num === sync2visible[i]){
+          var found = true;
+        }
+      }
+   
+      if(found===false){
+        if(num>sync2visible[sync2visible.length-1]){
+          sync2.trigger("owl.goTo", num - sync2visible.length+2)
+        }else{
+          if(num - 1 === -1){
+            num = 0;
+          }
+          sync2.trigger("owl.goTo", num);
+        }
+      } else if(num === sync2visible[sync2visible.length-1]){
+        sync2.trigger("owl.goTo", sync2visible[1])
+      } else if(num === sync2visible[0]){
+        sync2.trigger("owl.goTo", num-1)
+      }
+    }
+  },
   scrollCompany: function(){
     (function($){
       $(window).on("load",function(){
@@ -246,6 +316,7 @@ $(document).ready(function() {
   general.showBoxSearch();
   general.hideBoxSearch();
 
+  product.initImages();
   product.scrollCompany();
   product.initSliderPrice();
   product.initSliderPH();
