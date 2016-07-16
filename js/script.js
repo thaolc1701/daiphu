90db427f5be2cb583504a3e45535fd73e057fc03
ThaoLC <thaolc1701@gmail.com>
2016-07-16 20:37:03 +0700

    add image slider in project-details

--- js/script.js
+++ js/script.js
@@ -396,6 +396,80 @@ var product_details = {
   }
 }
 
+var project_details = {
+  initImages: function(){
+    var sync1 = $("#project-sync1");
+    var sync2 = $("#project-sync2");
+
+    sync1.owlCarousel_1({
+      singleItem : true,
+      slideSpeed : 1000,
+      navigation: false,
+      pagination:false,
+      afterAction : syncPosition,
+      responsiveRefreshRate : 200,
+    });
+
+    sync2.owlCarousel_1({
+      items : 4,
+      itemsDesktop      : [1199,4],
+      itemsDesktopSmall     : [979,4],
+      itemsTablet       : [768,4],
+      itemsMobile       : [479,3],
+      navigation: true,
+      pagination:false,
+      responsiveRefreshRate : 100,
+      afterInit : function(el){
+        el.find(".owl-item").eq(0).addClass("synced");
+      }
+    });
+
+    function syncPosition(el){
+      var current = this.currentItem;
+      $("#project-sync2")
+        .find(".owl-item")
+        .removeClass("synced")
+        .eq(current)
+        .addClass("synced")
+      if($("#project-sync2").data("owlCarousel_1") !== undefined){
+        center(current)
+      }
+    }
+
+    $("#project-sync2").on("click", ".owl-item", function(e){
+      e.preventDefault();
+      var number = $(this).data("owlItem");
+      sync1.trigger("owl.goTo",number);
+    });
+
+    function center(number){
+      var sync2visible = sync2.data("owlCarousel_1").owl.visibleItems;
+      var num = number;
+      var found = false;
+      for(var i in sync2visible){
+        if(num === sync2visible[i]){
+          var found = true;
+        }
+      }
+
+      if(found===false){
+        if(num>sync2visible[sync2visible.length-1]){
+          sync2.trigger("owl.goTo", num - sync2visible.length+2)
+        }else{
+          if(num - 1 === -1){
+            num = 0;
+          }
+          sync2.trigger("owl.goTo", num);
+        }
+      } else if(num === sync2visible[sync2visible.length-1]){
+        sync2.trigger("owl.goTo", sync2visible[1])
+      } else if(num === sync2visible[0]){
+        sync2.trigger("owl.goTo", num-1)
+      }
+    }
+  },
+}
+
 $(document).ready(function() {
 
   general.initBoxServiceCarousel();
@@ -417,5 +491,7 @@ $(document).ready(function() {
 
   product_details.initImages();
   product_details.initOtherProductsCarousel();
+
+  project_details.initImages();
 
 });
